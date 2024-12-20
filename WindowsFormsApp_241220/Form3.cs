ec02ad3feb4f6087759408e8a0929797c7b9b315
Ahyun-Song <yoon9113@naver.com>
2024-12-20 13:35:07 +0900

    RockScissorsPaper ammend

--- WindowsFormsApp_241220/Form3.cs
+++ WindowsFormsApp_241220/Form3.cs
@@ -15,10 +15,12 @@ namespace WindowsFormsApp_241220
 
         private int UserScore = 0;
         private int ComputerScore = 0;
+        private bool IsGameOver = false;
         public Form3()
         {
             InitializeComponent();
         }
+
 
         private void button_rock_Click(object sender, EventArgs e)
         {
@@ -37,12 +39,18 @@ namespace WindowsFormsApp_241220
 
         private void PlayGame(string UserChoice)
         {
+            if (IsGameOver)
+            {
+                textBox_result.Clear();
+                IsGameOver = false;
+            }
             Random random = new Random();
             string[] choices = { "Rock", "Scissors", "Paper" };
-            string ComputerChoice = choices[random.Next(0,3)];
+            string ComputerChoice = choices[random.Next(0, 3)];
 
             string result = GetGameResult(UserChoice, ComputerChoice);
 
+            // textBox_result.Clear();
             textBox_result.AppendText($"User : {UserChoice} | Computer : {ComputerChoice}\r\n");
             textBox_result.AppendText($"Result : {result}\r\n\r\n");
 
@@ -50,7 +58,7 @@ namespace WindowsFormsApp_241220
             {
                 UserScore++;
             }
-            else if(result == "Computer Wins")
+            else if (result == "Computer Wins")
             {
                 ComputerScore++;
             }
@@ -59,21 +67,24 @@ namespace WindowsFormsApp_241220
 
             if (UserScore == 3)
             {
-                textBox_result.AppendText("User Wins the Game");
+                textBox_result.AppendText("User Wins the Game\r\n\r\n");
                 ResetGame();
+                EndGame();
             }
             else if (ComputerScore == 3)
             {
-                textBox_result.AppendText("Computer Wins the Game");
+                textBox_result.AppendText("Computer Wins the Game\r\n\r\n");
                 ResetGame();
+                EndGame();
             }
+            // textBox_result.Clear();
 
-
+            
         }
 
         private string GetGameResult(string UserChoice, string ComputerChoice)
         {
-            if(UserChoice == ComputerChoice)
+            if (UserChoice == ComputerChoice)
             {
                 return "Draw";
             }
@@ -88,12 +99,16 @@ namespace WindowsFormsApp_241220
             }
 
         }
-
         private void ResetGame()
         {
             UserScore = 0;
             ComputerScore = 0;
-        }
 
+        }
+        private void EndGame()
+        {
+            IsGameOver = true;
+            //textBox_result.Clear();
+        }
     }
 }
