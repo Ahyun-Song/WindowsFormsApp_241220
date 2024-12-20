d1d3ff088ca0bd18253c9a13f8f0caaba87741a7
Ahyun-Song <yoon9113@naver.com>
2024-12-20 10:51:00 +0900

    Add project files.

---
+++ WindowsFormsApp_241220/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp_241220
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(13, 66);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(775, 359);
+            this.textBox_result.TabIndex = 0;
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(13, 22);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(775, 28);
+            this.textBox_input.TabIndex = 1;
+            this.textBox_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_input_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.TextBox textBox_input;
+    }
+}
+
---
+++ WindowsFormsApp_241220/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_241220
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int StudentCount;
+                if (int.TryParse(textBox_input.Text, out StudentCount))
+                {
+                    string[] results = GenerateStudentScores(StudentCount);
+
+                    textBox_result.Text = string.Join(Environment.NewLine, results);
+                }
+            }
+            
+           // e.SuppressKeyPress = true;
+        }
+        public string[] GenerateStudentScores(int count)
+        {
+            Random random = new Random();
+            string[] StudentScores = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int score = random.Next(0, 101);
+                StudentScores[i] = $"학생{i + 1}의 점수 : {score}점";
+            }
+            return StudentScores;
+        }
+       
+    }
+    
+}
